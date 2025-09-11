401ffd052b75f78656459e366132d87b91337f9f
Mahid-hub <mahidulislam128@gmail.com>
2025-09-11 20:29:01 +0500

    change in branch

--- src/SurahPageComponents/Verse.jsx
+++ src/SurahPageComponents/Verse.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Verse = ({
   verseNumber,
@@ -9,6 +9,11 @@ const Verse = ({
   bgClr,
   textClr,
   source,
+  id,
+  playingId,
+  setPlayingId,
+  className,
+  ...rest
 }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
@@ -19,78 +24,95 @@ const Verse = ({
     }
     if (isPlaying) {
       audioRef.current.pause();
+      setPlayingId(null);
+      setIsPlaying(false);
     } else {
       audioRef.current.play();
+      setPlayingId(id);
+      setIsPlaying(true);
     }
     setIsPlaying(!isPlaying);
   };
+
+  useEffect(() => {
+    if (playingId !== id && audioRef.current) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  }, [playingId, id]);
+
   return (
-    <div className={`${bgClr} ${textClr} px-6 py-6`}>
-      <audio
-        ref={audioRef}
-        src={source}
-        onEnded={() => setIsPlaying(false)}
-      ></audio>
-      <div className="flex items-start space-x-4 max-w-[1100px] mx-auto border-t-[1px] border-gray-500">
-        {/* ICONS - LEFT SIDE */}
-        <div className="flex-shrink-0">
-          <div className="flex flex-col space-y-4 text-gray-500">
-            <button
-              onClick={() => onCopy(verseNumber)}
-              className="p-2 hover:text-gray-300"
-              title="Copy verse"
-            >
-              <i class="fa-regular fa-copy"></i>
-            </button>
+    <div
+      className={`${bgClr} ${textClr} px-6 py-6 ${className || ""}`}
+      {...rest}
+    >
+      <div className={`${bgClr} ${textClr} px-6 py-6`}>
+        <audio
+          ref={audioRef}
+          src={source}
+          onEnded={() => setPlayingId(null)}
+        ></audio>
+        <div className="flex items-start space-x-4 max-w-[1100px] mx-auto border-t-[1px] border-gray-500">
+          {/* ICONS - LEFT SIDE */}
+          <div className="flex-shrink-0">
+            <div className="flex flex-col space-y-4 text-gray-500">
+              <button
+                onClick={() => onCopy(verseNumber)}
+                className="p-2 hover:text-gray-300"
+                title="Copy verse"
+              >
+                <i class="fa-regular fa-copy"></i>
+              </button>
 
-            <button
-              onClick={() => onBookmark(verseNumber)}
-              className="p-2 hover:text-gray-300"
-              title="Bookmark verse"
-            >
-              <i class="fa-regular fa-bookmark"></i>
-            </button>
+              <button
+                onClick={() => onBookmark(verseNumber)}
+                className="p-2 hover:text-gray-300"
+                title="Bookmark verse"
+              >
+                <i class="fa-regular fa-bookmark"></i>
+              </button>
 
-            <button
-              onClick={toggleClick}
-              className="p-2 hover:text-gray-300"
-              title="Play verse"
-            >
-              {isPlaying ? (
-                <>
-                  {" "}
-                  <i className="fa-regular fa-circle-pause"></i>
-                </>
-              ) : (
-                <>
-                  {" "}
-                  <i className="fa-regular fa-circle-play"></i>
-                </>
-              )}
-            </button>
+              <button
+                onClick={toggleClick}
+                className="p-2 hover:text-gray-300"
+                title="Play verse"
+              >
+                {isPlaying ? (
+                  <>
+                    {" "}
+                    <i className="fa-regular fa-circle-pause"></i>
+                  </>
+                ) : (
+                  <>
+                    {" "}
+                    <i className="fa-regular fa-circle-play"></i>
+                  </>
+                )}
+              </button>
 
-            <button className="p-2 hover:text-gray-300" title="Comment">
-              <i class="fa-regular fa-message"></i>
-            </button>
+              <button className="p-2 hover:text-gray-300" title="Comment">
+                <i class="fa-regular fa-message"></i>
+              </button>
 
-            <button className="p-2 hover:text-gray-300" title="More options">
-              <i class="fa-solid fa-ellipsis-vertical"></i>
-            </button>
-          </div>
-        </div>
-
-        {/* VERSE CONTENT */}
-        <div className="flex-1 font-arabic">
-          {/* Arabic text */}
-          <div className="text-right mt-3">
-            <p className="text-2xl leading-relaxed mb-2">
-              {arabicText} <span className="text-base">﴿{verseNumber}</span>
-            </p>
+              <button className="p-2 hover:text-gray-300" title="More options">
+                <i class="fa-solid fa-ellipsis-vertical"></i>
+              </button>
+            </div>
           </div>
 
-          {/* Translation */}
-          <div className="text-left">
-            <p className="leading-relaxed mb-4">{translation}</p>
+          {/* VERSE CONTENT */}
+          <div className="flex-1 font-arabic">
+            {/* Arabic text */}
+            <div className="text-right mt-3">
+              <p className="text-2xl leading-relaxed mb-2">
+                {arabicText} <span className="text-base">﴿{verseNumber}</span>
+              </p>
+            </div>
+
+            {/* Translation */}
+            <div className="text-left">
+              <p className="leading-relaxed mb-4">{translation}</p>
+            </div>
           </div>
         </div>
       </div>
